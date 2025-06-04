f2357e7a2be8850eb1a11f9fd6eb177a94e77bd2
Vladimir Rusakov <virt.p.q@yandex.ru>
2025-06-05 00:38:02 +0300

    add dockerfile with small settings fixes

--- LestaBackend/settings.py
+++ LestaBackend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-n9hdlywg^+f1i%@5)^)y07_$qlu0)w-a)rm2xsy9+cv8=v=-67"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
